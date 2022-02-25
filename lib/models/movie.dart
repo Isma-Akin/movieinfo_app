7ae40a976295b9e584f875b77c687e12b0d78db0
Isma-Akin <aakinsemoyin@hotmail.com>
2022-02-25 17:06:34 +0000

    feat(movie): add movies list

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -6,18 +6,125 @@ class Movie {
   final List<Map> cast;
 
   Movie({
-    this.poster,
-    this.title,
-    this.backdrop,
-    this.plot,
-    this.genre,
-    this.rating,
-    this.cast,
-    this.id,
-    this.year,
-    this.numOfRatings,
-    this.criticsReview,
-    this.metaScoreRating,
+    required this.poster,
+    required this.title,
+    required this.backdrop,
+    required this.plot,
+    required this.genre,
+    required this.rating,
+    required this.cast,
+    required this.id,
+    required this.year,
+    required this.numOfRatings,
+    required this.criticsReview,
+    required this.metaScoreRating,
 });
   }
 
+List<Movie> movies = [
+  Movie(
+    id: 1,
+    title: "Bloodshot",
+    year: 2020,
+    poster: "assets/images/poster_1.jpg",
+    backdrop: "assets/images/backdrop_1.jpg",
+    numOfRatings: 150212,
+    rating: 7.3,
+    criticsReview: 50,
+    metaScoreRating: 76,
+    genre: ["Action", "Drama"],
+    plot: plotText,
+    cast: [
+      {
+        "originalName": "James Mangold",
+        "movieName": "Director",
+        "image": "assets/images/actor_1.png"
+      },
+      {
+        "originalName": "Matt Damon",
+        "movieName": "Carroll",
+        "image": "assets/images/actor_2.png"
+      },
+      {
+        "originalName": "Christian Bale",
+        "movieName": "Ken Miles",
+        "image": "assets/images/actor_3.png"
+      },
+      {
+        "originalName": "Caitriona Balfe",
+        "movieName": "Mollie",
+        "image": "assets/images/actor_4.png"
+      },
+    ],
+  ),
+  Movie(
+    id: 2,
+    title: "Ford v Ferrari",
+    year: 2019,
+    poster: "assets/images/poster_2.jpg",
+    backdrop: "assets/images/backdrop_2.jpg",
+    numOfRatings: 150212,
+    rating: 8.2,
+    criticsReview: 50,
+    metaScoreRating: 76,
+    genre: ["Action", "Biography", "Drama"],
+    plot: plotText,
+    cast: [
+      {
+        "originalName": "James Mangold",
+        "movieName": "Director",
+        "image": "assets/images/actor_1.png"
+      },
+      {
+        "originalName": "Matt Damon",
+        "movieName": "Carroll",
+        "image": "assets/images/actor_2.png"
+      },
+      {
+        "originalName": "Christian Bale",
+        "movieName": "Ken Miles",
+        "image": "assets/images/actor_3.png"
+      },
+      {
+        "originalName": "Caitriona Balfe",
+        "movieName": "Mollie",
+        "image": "assets/images/actor_4.png"
+      },
+    ],
+  ),
+  Movie(
+    id: 1,
+    title: "Onward",
+    year: 2020,
+    poster: "assets/images/poster_3.jpg",
+    backdrop: "assets/images/backdrop_3.jpg",
+    numOfRatings: 150212,
+    rating: 7.6,
+    criticsReview: 50,
+    metaScoreRating: 79,
+    genre: ["Action", "Drama"],
+    plot: plotText,
+    cast: [
+      {
+        "originalName": "James Mangold",
+        "movieName": "Director",
+        "image": "assets/images/actor_1.png"
+      },
+      {
+        "originalName": "Matt Damon",
+        "movieName": "Carroll",
+        "image": "assets/images/actor_2.png"
+      },
+      {
+        "originalName": "Christian Bale",
+        "movieName": "Ken Miles",
+        "image": "assets/images/actor_3.png"
+      },
+      {
+        "originalName": "Caitriona Balfe",
+        "movieName": "Mollie",
+        "image": "assets/images/actor_4.png"
+      },
+    ],
+  ),
+];
