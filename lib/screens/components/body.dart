1e47720eb8ad4464b856967a022518c96053b378
Isma-Akin <aakinsemoyin@hotmail.com>
2022-02-25 20:27:56 +0000

    feat(body): add body stateless and stateful widget

---
+++ lib/screens/components/body.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
+
+class CategoryList extends StatefulWidget {
+  const CategoryList({Key? key}) : super(key: key);
+
+  @override
+  _CategoryListState createState() => _CategoryListState();
+}
+
+class _CategoryListState extends State<CategoryList> {
+  int selectedCategory = 0;
+  List<String> categories = ["In Theatre", "Box Office", "Coming Soon"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
