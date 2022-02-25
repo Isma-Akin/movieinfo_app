8a913edaaebebd115c992958964e97e23c476175
Isma-Akin <aakinsemoyin@hotmail.com>
2022-02-25 20:25:18 +0000

    refactor(home_screen): add scaffold widget with appbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:movieinfo_app/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        leading: IconButton(
+          padding: EdgeInsets.only(
+              left: kDefaultPadding),
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+          padding: EdgeInsets.symmetric(
+              horizontal: kDefaultPadding),
+            onPressed: () {},
+            icon: Icon(Icons.search))],
+      ),
+    );
   }
 }
