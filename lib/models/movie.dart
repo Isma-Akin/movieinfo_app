d8da758eb59718ed7c4fc19586e2f85bf11341e0
Isma-Akin <aakinsemoyin@hotmail.com>
2022-02-25 16:49:29 +0000

    feat(movie): create movie model

---
+++ lib/models/movie.dart
@@ -0,0 +1,23 @@
+class Movie {
+  final int id, year, numOfRatings, criticsReview, metaScoreRating;
+  final double rating;
+  final List<String> genre;
+  final String plot, title, poster, backdrop;
+  final List<Map> cast;
+
+  Movie({
+    this.poster,
+    this.title,
+    this.backdrop,
+    this.plot,
+    this.genre,
+    this.rating,
+    this.cast,
+    this.id,
+    this.year,
+    this.numOfRatings,
+    this.criticsReview,
+    this.metaScoreRating,
+});
+  }
+
